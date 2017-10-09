50d29d3498daf7149f428c99d91ccd00d75e2834
iranna.patil@irdeto.com <iranna.patil@irdeto.com>
2017-10-09 16:17:17 +0200

    Adding jndi data source support

--- application.sh
+++ application.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env bash
+# Retrieving local ip. "grep -v link" is added to prevent VPN tunnel getting in the way
+DEFAULT_GATEWAY=$(ip route | grep default | grep -v link | head -1 | awk '{print $3}')
+IP_ADDRESS_ON_LAN=$(ip route get ${DEFAULT_GATEWAY} | grep src | awk '{print $5}')
+# IP to export JMX and other ports
+export HOST_IP="$IP_ADDRESS_ON_LAN"
+
 export POSTGRES_IMAGE="docker-ngmpp-prod.artifactory.engineering-hdc.net/library/postgres:9.5.5-5"
 export APPLICATION_IMAGE="identity-server"
 export POSTGRES_PORT=1111
 export APPLICATION_PORT=9876
+export POSTGRES_DB_NAME=identity_service
+export POSTGRES_UNAME=identity
+export POSTGRES_PASSWORD=changeme001
+
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 COMPOSE_FILE_PATH="${SCRIPT_DIR}/deploy/docker-compose.yml"
